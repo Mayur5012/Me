f503141bec72b984c64fe43a282519aa5c427026
Mayuresh Gawali <mayureshgawali9@gmail.com>
2023-03-29 19:46:53 +0530

    responsive for tabs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,24 +5,29 @@ import { SocialIcon } from 'react-social-icons';
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <img src={foot} alt="" style={{ width: "100%" }} />
-      <div className="f-content">
-        {/* <span>Get In Touch</span> */}
-        <div className="f-icons">
-          {/* social icons */}
-        <div className="i-icons">
-        <SocialIcon url="https://www.linkedin.com/in/mayureshgawali/" target={"blank"} bgColor="white" style={{ height: 100, width: 100 }} />
-        <SocialIcon url="https://github.com/Mayur5012" bgColor="white" target={"blank"}  style={{ height: 100, width: 100 }} />
-        <SocialIcon url="https://www.instagram.com/array_mayur/" bgColor="white" target={"blank"}  style={{ height: 100, width: 100 }} />
-        <SocialIcon url="https://wa.me/8505890182"  target={"blank"}  bgColor="white" style={{ height: 100, width: 100 }} />
-        <SocialIcon url="https://mail.google.com/mail/u/0/?fs=1&to=mayureshgawali9@gmail.com&tf=cm"  target={"blank"}  bgColor="white" style={{ height: 100, width: 100 }} />
-        </div>
-        </div>
-        <p>© 2023 Mayuresh Gawali | Made with ♡ </p>
-      </div>
+<>
+<div className="footer">
+<p>© 2023 Mayuresh Gawali | Made with ♡ </p>
+</div>
+</>
+    // <div className="footer">
+    //   <img src={foot} alt="" style={{ width: "100%" }} />
+    //   <div className="f-content">
+    //     {/* <span>Get In Touch</span> */}
+    //     <div className="f-icons">
+    //       {/* social icons */}
+    //     <div className="i-icons">
+    //     <SocialIcon url="https://www.linkedin.com/in/mayureshgawali/" target={"blank"} bgColor="white" style={{ height: 100, width: 100 }} />
+    //     <SocialIcon url="https://github.com/Mayur5012" bgColor="white" target={"blank"}  style={{ height: 100, width: 100 }} />
+    //     <SocialIcon url="https://www.instagram.com/array_mayur/" bgColor="white" target={"blank"}  style={{ height: 100, width: 100 }} />
+    //     <SocialIcon url="https://wa.me/8505890182"  target={"blank"}  bgColor="white" style={{ height: 100, width: 100 }} />
+    //     <SocialIcon url="https://mail.google.com/mail/u/0/?fs=1&to=mayureshgawali9@gmail.com&tf=cm"  target={"blank"}  bgColor="white" style={{ height: 100, width: 100 }} />
+    //     </div>
+    //     </div>
+    //     <p>© 2023 Mayuresh Gawali | Made with ♡ </p>
+    //   </div>
       
-    </div>
+    // </div>
   );
 };
 
