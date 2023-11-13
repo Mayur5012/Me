9a37d75d657ce777b54708a6022875195e89f076
Mayuresh Gawali <mayureshgawali9@gmail.com>
2023-11-13 10:28:53 +0530

    download update

--- src/components/Skills/Skill.js
+++ src/components/Skills/Skill.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import './Skill.css'
-import designer from '../../img/designer.png'
-import developer from '../../img/developer.png'
-import programmer from '../../img/programmer.png'
+import React from "react";
+import "./Skill.css";
+import designer from "../../img/designer.png";
+import developer from "../../img/developer.png";
+import programmer from "../../img/programmer.png";
 
 import Typical from "react-typical";
-import Resume from './Mayuresh Gawali Resume.pdf';
+import Resume from "./Mayuresh Gawali Resume.pdf";
+import Cocubes from "./cocubes.pdf";
 const Skill = () => {
   return (
     <>
-    <div className='skillContainer'>
-        <span className='heading'>What I Do!</span>
+      <div className="skillContainer">
+        <span className="heading">What I Do!</span>
         <span className="typical">
+          {" "}
+          <h4 style={{ textAlign: "center", color: "#43ddff" }}>
             {" "}
-            <h4 style={{textAlign:"center", color:"#43ddff"}}>
-              {" "}
-              <Typical 
-              loop ={Infinity}
+            <Typical
+              loop={Infinity}
               steps={[
                 " ",
                 500,
@@ -27,34 +28,46 @@ const Skill = () => {
                 "Practicing data structures and algorithms everyday!",
                 1000,
               ]}
-              />
-            </h4>
-          </span>
-          <a style={{textAlign:"center"}} href={Resume} download>
-          <button className="buttonResume">Download CV</button>
-        </a>
-    <div className='skill'>
-        <div className='skillCard'>
-            <img src={designer} alt=""/>
-            <h4 id='skillHeading'>Design</h4>
-            <span className='skillList'>Figma, Canva, Adobe Illustrator, VistaCreate</span>
+            />
+          </h4>
+        </span>
+        <div className="downloadPDF">
+          <div style={{marginRight:"20px"}}>
+          <a style={{ textAlign: "center" }} href={Resume} download>
+            <button className="buttonResume">Download Resume</button>
+          </a>
+          </div>
+          <div>
+          </div>
+          <a style={{ textAlign: "center" }} href={Cocubes} download>
+            <button className="buttonResume">Download Cocubes Report</button>
+          </a>
         </div>
-        <div className='skillCard'>
-            <img src={developer} alt=""/>
-            <h4 id='skillHeading'>Developer</h4>
-            <span className='skillList'>JavaScript, ReactJs, NodeJs, ExpressJs, ML/AI, Html, CSS3, Bootstrap, PHP, AWS, MongoDb, MySQL </span>
+        <div className="skill">
+          <div className="skillCard">
+            <img src={designer} alt="" />
+            <h4 id="skillHeading">Design</h4>
+            <span className="skillList">
+              Figma, Canva, Adobe Illustrator, VistaCreate
+            </span>
+          </div>
+          <div className="skillCard">
+            <img src={developer} alt="" />
+            <h4 id="skillHeading">Developer</h4>
+            <span className="skillList">
+              JavaScript, ReactJs, NodeJs, ExpressJs, ML/AI, Html, CSS3,
+              Bootstrap, PHP, AWS, MongoDb, MySQL{" "}
+            </span>
+          </div>
+          <div className="skillCard">
+            <img src={programmer} alt="" />
+            <h4 id="skillHeading">Programming</h4>
+            <span className="skillList">C++, Java, Python</span>
+          </div>
         </div>
-        <div className='skillCard'>
-            <img src={programmer} alt=""/>
-            <h4 id='skillHeading'>Programming</h4>
-            <span className='skillList'>C/C++, Java, Python</span>
-        </div>
-        
-    </div> 
-    </div>
-      
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Skill
+export default Skill;
